38e1800880d56211d3568de6090bff893e0fafc0
Robb Hamilton <rhamilto@redhat.com>
2015-04-08 16:31:10 -0400

    Changes based on usability testing; drag and drop improvements

--- dist/js/rcue-rdom.js
+++ dist/js/rcue-rdom.js
@@ -6,13 +6,37 @@ jQuery(function () {
   // Initialize Boostrap-select
   jQuery('.selectpicker').selectpicker();
   // Make configured deployment roles draggable
-  jQuery('.deployment-roles li:not(.role-unconfigured)').draggable({ 
-    stack: ".deployment-roles li"
+  jQuery('.deployment-roles-assignable li:not(.role-unconfigured)').draggable({
+    helper: 'clone',
+    opacity: .75,
+    revert: 'invalid',
+    snap: '.role-target',
+    snapMode: 'inner',
+    zIndex: 1000
   });
-  // Make .role
-  jQuery('.role-target').droppable({
-    drop: function( event, ui ) {
-      jQuery(this).addClass('ui-state-highlight')
+  jQuery('.deployment-roles-unassigned').droppable({
+    accept: '.deployment-roles li',
+    activeClass: 'deployment-roles-active',
+    hoverClass: 'deployment-roles-hover',
+    tolerance: 'touch',
+    drop: function (ev, ui) {
+      ui.draggable.removeClass('role-assigned');
+      ui.draggable.prependTo(jQuery(this));
+      //jQuery('.deployment-roles-unassigned .no-roles').toggle();
     }
   });
-})
+  jQuery('.role-target').droppable({
+    accept: '.deployment-roles li',
+    activeClass: 'role-target-active',
+    hoverClass: 'role-target-hover',
+    tolerance: 'intersect',
+    drop: function (ev, ui) {
+      ui.draggable.addClass('role-assigned');
+      ui.draggable.prependTo(jQuery(this).parent('.deployment-roles'));
+    }
+  });
+  jQuery('.role-option.delete').click(function() {
+    jQuery(this).parent('.role').removeClass('role-assigned');
+    jQuery(this).parent('.role').appendTo('.deployment-roles-unassigned');
+  });
+});
